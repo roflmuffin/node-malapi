c70d2e9e48b955dbb07ee6f181471631f76cdf7e
Roflmuffin <shortguy014@gmail.com>
2016-08-21 22:20:14 +1000

    Added comments to helper test file
    (Dummy commit to test Travis caching)

--- test/helper.js
+++ test/helper.js
@@ -3,7 +3,9 @@ module.exports = {
   TEST_ANIME_NAME: process.env.TEST_ANIME_NAME || 'Haikyuu!! Second Season',
   TEST_ANIME_URL: process.env.TEST_ANIME_URL || '/anime/28891/',
 
+  // Should match the name of the first episode of anime above.
   TEST_EPISODE_NAME: process.env.TEST_EPISODE_NAME || 'Let\'s Go to Tokyo!',
 
+  // Test for anime that contains missing sidebar information.
   TEST_ANIME_MISSINGINFO: process.env.TEST_ANIME_MISSINGINFO || 32998,
 };
