e990cceadb5f4eafbc57b8c16bdbf72adae746e2
Jason "Don" O'Conal <lovek323@gmail.com>
2016-09-27 09:38:08 +0930

    Handle missing dates

--- src/models/episode.js
+++ src/models/episode.js
@@ -22,20 +22,23 @@ class Episode {
       .match(/((?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]+, [0-9]{4})\(([A-Z]+)\)/);
     var timeZone = 'GMT';
 
-    switch (airedMatch[2]) {
-      case 'JST':
-        timeZone = 'GMT+09:00';
-        break;
-    }
-
     const result = {
       number: parseInt($('table h2.fs18 span').text().replace(/[^\d\.\-]/g, ''), 10),
       name: $('table h2.fs18').get(0).childNodes[2].nodeValue,
       alt: $('table h2.fs18').siblings('p').text().trim(),
       synopsis: $("h2:contains('Synopsis')").parent().text().replace('Synopsis', '')
-        .trim(),
-      aired: new Date(airedMatch[1] + ' 00:00:00 ' + timeZone)
+        .trim()
     };
+
+
+    if (airedMatch !== null) {
+      switch (airedMatch[2]) {
+        case 'JST':
+          timeZone = 'GMT+09:00';
+          break;
+      }
+      result.aired = new Date(airedMatch[1] + ' 00:00:00 ' + timeZone);
+    }
 
     return new Episode(result);
   }
