f4afbf2b24760131a3ec1a4116b90cacc6c9a4d0
Roflmuffin <shortguy014@gmail.com>
2016-11-02 20:44:14 +1000

    Fixed parsing util & new NPM version

--- src/util/parsing.js
+++ src/util/parsing.js
@@ -1,10 +1,13 @@
 function parseSidebar($, term, split = false) {
   const el = $(`span:contains("${term}:")`);
+  const re = new RegExp(`${term}:\n`, 'g');
+
   if (typeof el !== 'undefined' && el.length > 0) {
     if (split) {
-      return el.next().text().trim().split(',');
+      return el.parent().text().replace(re, '').trim()
+        .split(',');
     }
-    return el.next().text().trim();
+    return el.parent().text().replace(re, '').trim();
   }
   return null;
 }
